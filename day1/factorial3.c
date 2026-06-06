099f5d1e102c4f93039d41832107a4a3776794e8
Aditi Singh <singhaditi2506@gmail.com>
2026-06-06 13:00:47 +0530

    Day 1 assingnement

---
+++ day1/factorial3.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int n,i;
+    int fact=1;
+    printf("enter the number");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        fact =fact*i;
+    }
+    printf("factorial of %d is  %d",n,fact);
+    return 0;
+}
